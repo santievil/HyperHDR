d206e9c0bf552357a00a4b9641d67b22bb1a1d4f
santievil <santi_evil@yahoo.com>
2025-02-17 09:04:55 +0100

    t1

--- sources/grabber/linux/framebuffer/FrameBufGrabber.cpp
+++ sources/grabber/linux/framebuffer/FrameBufGrabber.cpp
@@ -56,7 +56,7 @@ const char DEFAULT_CAPTURE_DEVICE[] = "/dev/amvideocap0";
 typedef int64_t LONG_PTR, * PLONG_PTR;
 typedef LONG_PTR SSIZE_T, * PSSIZE_T;
 //typedef SSIZE_T AMLssize_t;
-typedef SSIZE_T ssize_t;
+//typedef SSIZE_T ssize_t;
 
 FrameBufGrabber::FrameBufGrabber(const QString& device, const QString& configurationPath)
 	: Grabber(configurationPath, "FRAMEBUFFER_SYSTEM:" + device.left(14))
@@ -320,7 +320,7 @@ void FrameBufGrabber::grabFrame()
 					int _bytesPerPixel = 3; // Valor por defecto (BGR24)
 
 					// Leer el frame
-					AMLssize_t bytesRead = pread(_captureDev, _image_ptr, _bytesToRead, 0);
+					ssize_t bytesRead = pread(_captureDev, _image_ptr, _bytesToRead, 0);
 
 					if (bytesRead < 0 && errno != EAGAIN && errno > 0)
 					{
@@ -329,7 +329,7 @@ void FrameBufGrabber::grabFrame()
 					}
 					else
 					{
-						if (bytesRead != -1 && static_cast<AMLssize_t>(_bytesToRead) != bytesRead)
+						if (bytesRead != -1 && static_cast<ssize_t>(_bytesToRead) != bytesRead)
 						{
 							ErrorIf(_lastError != 4, _log, "Capture failed to grab entire image [bytesToRead(%zu) != bytesRead(%zd)]", _bytesToRead, bytesRead);
 							_lastError = 4;
