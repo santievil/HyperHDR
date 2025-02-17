b906bf106fcb06cb27324bbf17619e1c162166e5
santievil <santi_evil@yahoo.com>
2025-02-17 08:44:30 +0100

    t1

--- include/utils/ColorRgbAML.h
+++ include/utils/ColorRgbAML.h
@@ -12,7 +12,7 @@
 /// Plain-Old-Data structure containing the red-green-blue color specification. Size of the
 /// structure is exactly 3-bytes for easy writing to led-device
 ///
-struct ColorRgb
+struct ColorRgbAML
 {
 	/// The red color channel
 	uint8_t red;
@@ -22,25 +22,25 @@ struct ColorRgb
 	uint8_t blue;
 
 	/// 'Black' RgbColor (0, 0, 0)
-	static const ColorRgb BLACK;
+	static const ColorRgbAML BLACK;
 	/// 'Red' RgbColor (255, 0, 0)
-	static const ColorRgb RED;
+	static const ColorRgbAML RED;
 	/// 'Green' RgbColor (0, 255, 0)
-	static const ColorRgb GREEN;
+	static const ColorRgbAML GREEN;
 	/// 'Blue' RgbColor (0, 0, 255)
-	static const ColorRgb BLUE;
+	static const ColorRgbAML BLUE;
 	/// 'Yellow' RgbColor (255, 255, 0)
-	static const ColorRgb YELLOW;
+	static const ColorRgbAML YELLOW;
 	/// 'White' RgbColor (255, 255, 255)
-	static const ColorRgb WHITE;
+	static const ColorRgbAML WHITE;
 	/// 'Cyan' RgbColor (0, 255, 255)
-	static const ColorRgb CYAN;
+	static const ColorRgbAML CYAN;
 	/// 'Magenta' RgbColor (255, 0,255)
-	static const ColorRgb MAGENTA;
+	static const ColorRgbAML MAGENTA;
 
-	ColorRgb() = default;
+	ColorRgbAML() = default;
 
-	ColorRgb(uint8_t _red, uint8_t _green,uint8_t _blue):
+	ColorRgbAML(uint8_t _red, uint8_t _green,uint8_t _blue):
 		  red(_red),
 		  green(_green),
 		  blue(_blue)
@@ -48,9 +48,9 @@ struct ColorRgb
 
 	}
 
-	ColorRgb operator-(const ColorRgb& b) const
+	ColorRgbAML operator-(const ColorRgbAML& b) const
 	{
-		ColorRgb a(*this);
+		ColorRgbAML a(*this);
 		a.red -= b.red;
 		a.green -= b.green;
 		a.blue -= b.blue;
@@ -76,16 +76,16 @@ struct ColorRgb
 };
 
 /// Assert to ensure that the size of the structure is 'only' 3 bytes
-static_assert(sizeof(ColorRgb) == 3, "Incorrect size of ColorRgb");
+static_assert(sizeof(ColorRgbAML) == 3, "Incorrect size of ColorRgbAML");
 
 ///
-/// Stream operator to write ColorRgb to an outputstream (format "'{'[red]','[green]','[blue]'}'")
+/// Stream operator to write ColorRgbAML to an outputstream (format "'{'[red]','[green]','[blue]'}'")
 ///
 /// @param os The output stream
 /// @param color The color to write
 /// @return The output stream (with the color written to it)
 ///
-inline std::ostream& operator<<(std::ostream& os, const ColorRgb& color)
+inline std::ostream& operator<<(std::ostream& os, const ColorRgbAML& color)
 {
 	os << "{"
 	   << static_cast<unsigned>(color.red) << ","
@@ -97,13 +97,13 @@ inline std::ostream& operator<<(std::ostream& os, const ColorRgb& color)
 }
 
 ///
-/// Stream operator to write ColorRgb to a QTextStream (format "'{'[red]','[green]','[blue]'}'")
+/// Stream operator to write ColorRgbAML to a QTextStream (format "'{'[red]','[green]','[blue]'}'")
 ///
 /// @param os The output stream
 /// @param color The color to write
 /// @return The output stream (with the color written to it)
 ///
-inline QTextStream& operator<<(QTextStream &os, const ColorRgb& color)
+inline QTextStream& operator<<(QTextStream &os, const ColorRgbAML& color)
 {
 	os << "{"
 	   << static_cast<unsigned>(color.red) << ","
@@ -115,7 +115,7 @@ inline QTextStream& operator<<(QTextStream &os, const ColorRgb& color)
 }
 
 /// Compare operator to check if a color is 'equal' to another color
-inline bool operator==(const ColorRgb & lhs, const ColorRgb & rhs)
+inline bool operator==(const ColorRgbAML & lhs, const ColorRgbAML & rhs)
 {
 	return	lhs.red   == rhs.red   &&
 		lhs.green == rhs.green &&
@@ -123,7 +123,7 @@ inline bool operator==(const ColorRgb & lhs, const ColorRgb & rhs)
 }
 
 /// Compare operator to check if a color is 'smaller' than another color
-inline bool operator<(const ColorRgb & lhs, const ColorRgb & rhs)
+inline bool operator<(const ColorRgbAML & lhs, const ColorRgbAML & rhs)
 {
 	return	lhs.red   < rhs.red   &&
 		lhs.green < rhs.green &&
@@ -131,13 +131,13 @@ inline bool operator<(const ColorRgb & lhs, const ColorRgb & rhs)
 }
 
 /// Compare operator to check if a color is 'not equal' to another color
-inline bool operator!=(const ColorRgb & lhs, const ColorRgb & rhs)
+inline bool operator!=(const ColorRgbAML & lhs, const ColorRgbAML & rhs)
 {
 	return !(lhs == rhs);
 }
 
 /// Compare operator to check if a color is 'smaller' than or 'equal' to another color
-inline bool operator<=(const ColorRgb & lhs, const ColorRgb & rhs)
+inline bool operator<=(const ColorRgbAML & lhs, const ColorRgbAML & rhs)
 {
 	return	lhs.red   <= rhs.red   &&
 		lhs.green <= rhs.green &&
@@ -145,7 +145,7 @@ inline bool operator<=(const ColorRgb & lhs, const ColorRgb & rhs)
 }
 
 /// Compare operator to check if a color is 'greater' to another color
-inline bool operator>(const ColorRgb & lhs, const ColorRgb & rhs)
+inline bool operator>(const ColorRgbAML & lhs, const ColorRgbAML & rhs)
 {
 	return	lhs.red   > rhs.red   &&
 		lhs.green > rhs.green &&
@@ -153,7 +153,7 @@ inline bool operator>(const ColorRgb & lhs, const ColorRgb & rhs)
 }
 
 /// Compare operator to check if a color is 'greater' than or 'equal' to another color
-inline bool operator>=(const ColorRgb & lhs, const ColorRgb & rhs)
+inline bool operator>=(const ColorRgbAML & lhs, const ColorRgbAML & rhs)
 {
 	return	lhs.red   >= rhs.red   &&
 		lhs.green >= rhs.green &&
