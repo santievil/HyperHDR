b1b2f88cb5a6ce27fb764ac85ce418727d12f791
santievil <santi_evil@yahoo.com>
2025-02-24 11:03:02 +0100

    t1

--- sources/grabber/linux/framebuffer/FrameBufGrabber.cpp
+++ sources/grabber/linux/framebuffer/FrameBufGrabber.cpp
@@ -445,35 +445,38 @@ bool FrameBufGrabber::grabFrameAmlogic()
 
 				//return true;
 
-
-				if ((currentTime - lastCaptureTime < 5) && (savedImages.size() >= MAX_IMAGES))
+				currentTime = std::time(nullptr);
+				if ((currentTime - lastCaptureTime < 5) || (savedImages.size() >= MAX_IMAGES))
 				{
 					return true;
 				}
-				lastCaptureTime = currentTime;
+				else {
+					lastCaptureTime = currentTime;
 
-				std::tm tm = *std::localtime(&currentTime);
-				std::ostringstream filename;
-				//filename << "capture_" << std::put_time(&tm, "%y%m%d%H%M%S") << ".rgb";
-				filename << "/storage/.kodi/temp/capture_" << std::put_time(&tm, "%y%m%d%H%M%S") << ".rgb";
+					std::tm tm = *std::localtime(&currentTime);
+					std::ostringstream filename;
+					//filename << "capture_" << std::put_time(&tm, "%y%m%d%H%M%S") << ".rgb";
+					filename << "/storage/.kodi/temp/capture_" << std::put_time(&tm, "%y%m%d%H%M%S") << ".rgb";
 
-				/*if (savedImages.size() >= MAX_IMAGES)
-				{
-					std::filesystem::remove(savedImages.front());
-					savedImages.erase(savedImages.begin());
-				}*/
+					/*if (savedImages.size() >= MAX_IMAGES)
+					{
+						std::filesystem::remove(savedImages.front());
+						savedImages.erase(savedImages.begin());
+					}*/
 
-				std::ofstream outFile(filename.str(), std::ios::binary);
-				if (outFile.is_open())
-				{
-					outFile.write(reinterpret_cast<char*>(_image_ptr), bytesRead);
-					outFile.close();
-					savedImages.push_back(filename.str());					
-					Info(_log, "Saved captured frame to %s", filename.str().c_str());
-				}
-				else
-				{
-					Error(_log, "Failed to open file %s for writing", filename.str().c_str());
+					std::ofstream outFile(filename.str(), std::ios::binary);
+					if (outFile.is_open())
+					{
+						outFile.write(reinterpret_cast<char*>(_image_ptr), bytesRead);
+						outFile.close();
+						savedImages.push_back(filename.str());
+						Info(_log, "Saved captured frame to %s", filename.str().c_str());
+					}
+					else
+					{
+						Error(_log, "Failed to open file %s for writing", filename.str().c_str());
+					}
+
 				}
 				return true;
 			}
