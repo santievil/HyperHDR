eecf98ec3dae1527ed902bafa47b14df4a2d41a7
santievil <santi_evil@yahoo.com>
2025-03-11 10:58:40 +0100

    t1

--- sources/grabber/linux/framebuffer/FrameBufGrabber.cpp
+++ sources/grabber/linux/framebuffer/FrameBufGrabber.cpp
@@ -415,7 +415,9 @@ bool FrameBufGrabber::grabFrameAmlogic()
 	long r4 = ioctl(_captureDev, AMVIDEOCAP_IOW_SET_WANTFRAME_WAIT_MAX_MS, AMVIDEOCAP_WAIT_MAX_MS);
 
 	unsigned int frame_format;
+	unsigned int scframe_format;
 	long r5 = ioctl(_captureDev, AMVIDEOCAP_IOR_GET_FRAME_FORMAT, &frame_format);
+	long r6 = ioctl(_captureDev, AMVIDEOCAP_IOR_GET_SRCFRAME_FORMAT, &scframe_format);
 
 	if (r1 < 0 || r2 < 0 || r3 < 0 || r4 < 0 || _height == 0 || _width == 0)
 	{
@@ -462,6 +464,9 @@ bool FrameBufGrabber::grabFrameAmlogic()
 				{
 					if (r5 > 0) {
 						Warning(_log, "Formato de frame: 0x%x\n", frame_format);
+					}
+					if (r6 > 0) {
+						Warning(_log, "Formato de scframe: 0x%x\n", scframe_format);
 					}
 					processSystemFrameBGR(static_cast<uint8_t*>(base), linelen);
 					free(base);
