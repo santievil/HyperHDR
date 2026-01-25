b3bd1ee3be8e52204e182ee451b9cb9e838bbc8a
santievil <santi_evil@yahoo.com>
2026-01-25 21:39:06 +0100

    Funciones28

--- sources/base/Grabber.cpp
+++ sources/base/Grabber.cpp
@@ -687,16 +687,16 @@ void Grabber::processSystemFrameBGR(uint8_t* source, int lineSize)
 	int divide = getTargetSystemFrameDimension(targetSizeX, targetSizeY);
 	Image<ColorRgb> image(targetSizeX, targetSizeY);
 
-	Info(_log, "Primeros bytes source: {:02X} {:02X} {:02X} {:02X} {:02X} {:02X}", source[0], source[1], source[2], source[3], source[4], source[5]);
+	//Info(_log, "Primeros bytes source: {:02X} {:02X} {:02X} {:02X} {:02X} {:02X}", source[0], source[1], source[2], source[3], source[4], source[5]);
 
 	FrameDecoder::processSystemImageBGR(image, targetSizeX, targetSizeY, _cropLeft, _cropTop, source, _actualWidth, _actualHeight, divide, (_hdrToneMappingEnabled == 0 || !_lutBufferInit) ? nullptr : _lut.data(), lineSize);
 
 	if (getHdrToneMappingEnabled())
 	{
-		Info(_log, "Aplicamos LUT a la imagen ya RGB");
+		/*Info(_log, "Aplicamos LUT a la imagen ya RGB");
 		Info(_log, "HDR={} LUT loaded={} size={}", getHdrToneMappingEnabled(), _lut.data() != nullptr, _lut.size());
 		Info(_log, "Primeros pixels: {:02X} {:02X} {:02X} {:02X} {:02X} {:02X}", image.rawMem()[0], image.rawMem()[1], image.rawMem()[2], image.rawMem()[3], image.rawMem()[4], image.rawMem()[5]);
-		Info(_log, "AML Frame info: bytes=%zu width=%d height=%d lineSize=%d", lineSize * _actualHeight, _width, _height, lineSize);
+		Info(_log, "AML Frame info: bytes=%zu width=%d height=%d lineSize=%d", lineSize * _actualHeight, _width, _height, lineSize);*/
 
 		FrameDecoder::applyLUT(image.rawMem(), image.width(), image.height(), _lut.data(), getHdrToneMappingEnabled());
 	}
