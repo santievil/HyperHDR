b8f3d43573dae9dfc8cc6fb09837890fec3531f3
santievil <santi_evil@yahoo.com>
2026-01-26 12:04:30 +0100

    Funciones36

--- sources/utils/FrameDecoder.cpp
+++ sources/utils/FrameDecoder.cpp
@@ -526,7 +526,7 @@ void FrameDecoder::processSystemImageBGR(Image<ColorRgb>& image, int targetSizeX
             uvPlane[uvIndex + 1] = (uint8_t)(sumV / 4);
         }
     }
-	outputImage.setOriginFormat(PixelFormat::NV12);
+	image.setOriginFormat(PixelFormat::NV12);
 }
 
 
