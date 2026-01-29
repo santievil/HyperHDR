f8ec05fde44c3e2a23ddfb811cc7171f5ee4fbd6
santievil <santi_evil@yahoo.com>
2026-01-29 21:32:51 +0100

    LUTDELAY2

--- include/infinite-color-engine/InfiniteSmoothing.h
+++ include/infinite-color-engine/InfiniteSmoothing.h
@@ -81,8 +81,8 @@ private:
 		float		  smoothingFactor;
 		float		  stiffness;
 		float		  damping;
-		float		  y_limit;
 		float		  updateDelayFrames;
+		float		  y_limit;
 	};
 
 	std::vector<std::unique_ptr<SmoothingConfig>> _configurations;
