674488891eb6f884846e461877671fe746af7b74
santievil <santi_evil@yahoo.com>
2026-02-01 00:56:59 +0100

    LUTCAL6

--- sources/lut-calibrator/LutCalibrator.cpp
+++ sources/lut-calibrator/LutCalibrator.cpp
@@ -373,6 +373,15 @@ bool LutCalibrator::set1to1LUT()
                     _lut.data()[ind_lutd]     = std::clamp((int)(yuv.x + 0.5), 0, 255);
                     _lut.data()[ind_lutd + 1] = std::clamp((int)(yuv.y + 0.5), 0, 255);
                     _lut.data()[ind_lutd + 2] = std::clamp((int)(yuv.z + 0.5), 0, 255);
+
+					 // LOG para White [255, 255, 255]
+                    if (r == 255 && g == 255 && b == 255)
+                    {
+                        Info(_log, "LUT[255,255,255] = Y:{:d} U:{:d} V:{:d}", 
+                             _lut.data()[ind_lutd], 
+                             _lut.data()[ind_lutd + 1], 
+                             _lut.data()[ind_lutd + 2]);
+                    }
                 }
 
         emit GlobalSignals::getInstance()->SignalSetLut(&_lut);
