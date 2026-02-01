1420348a1e26c14ec7ac52a57794e029ab656398
santievil <santi_evil@yahoo.com>
2026-02-02 00:04:29 +0100

    LUTCAL6

--- sources/grabber/linux/amlogic/AmlogicGrabber.cpp
+++ sources/grabber/linux/amlogic/AmlogicGrabber.cpp
@@ -677,6 +677,7 @@ void AmlogicGrabber::signalSetLutHandler(MemoryBuffer<uint8_t>* lut)
 		_lutBufferInit = true;
 		_hdrToneMappingEnabled = 1;
 		Info(_log, "Amlogic The byte array loaded into LUT");
+		calibrationData.isRunning = true;
 	}
 	else
 		Error(_log, "Vengo de Amlogic. Could not set LUT: current size = {:d}, incoming size = {:d}", _lut.size(), (lut != nullptr) ? lut->size() : 0);
