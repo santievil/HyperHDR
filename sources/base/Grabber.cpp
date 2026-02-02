7c83d87252358a3ef14ac6a3b6bcb971f9a063fd
santievil <santi_evil@yahoo.com>
2026-02-02 22:25:31 +0100

    LUTCAL6

--- sources/base/Grabber.cpp
+++ sources/base/Grabber.cpp
@@ -703,7 +703,7 @@ void Grabber::processSystemFrameBGR(uint8_t* source, int lineSize)
 	}*/
 
 	//FrameDecoder::processSystemImageBGRCal(image, targetSizeX, targetSizeY, _cropLeft, _cropTop, source, _actualWidth, _actualHeight, divide, (_hdrToneMappingEnabled == 0 || !_lutBufferInit) ? nullptr : _lut.data(), lineSize);
-	FrameDecoder::processSystemImageBGRCal(image, targetSizeX, targetSizeY, _cropLeft, _cropTop, source, _actualWidth, _actualHeight, divide, (_hdrToneMappingEnabled == 0 || !_lutBufferInit) ? nullptr : _lut.data(), lineSize);
+	FrameDecoder::processSystemImageBGR(image, targetSizeX, targetSizeY, _cropLeft, _cropTop, source, _actualWidth, _actualHeight, divide, (_hdrToneMappingEnabled == 0 || !_lutBufferInit) ? nullptr : _lut.data(), lineSize);
 
 
 	if (_signalDetectionEnabled)
